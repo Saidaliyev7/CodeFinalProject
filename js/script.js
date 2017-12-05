404fe30cb9a89d4957f4aa8f84224e847397455f
Said <mrsaidaliyev@hotmail.com>
2017-12-05 23:01:18 +0400

    homeda iki section elemiwem 3cu slideri edirem

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
 $(document).ready(function () {
+    $(".owl-carousel").owlCarousel( {
+        loop:true,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+    $( ".owl-prev").html('<i class="fa fa-arrow-left"></i>');
+    $( ".owl-next").html('<i class="fa fa-arrow-right"></i>');
 
 });
