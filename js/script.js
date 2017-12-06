1af17438ce75ea6f806e75be193100378371bf1a
Said <mrsaidaliyev@hotmail.com>
2017-12-06 11:39:01 +0400

    code buttonu-index.scss slider done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $(".owl-carousel").owlCarousel( {
+    $(".category .owl-carousel").owlCarousel( {
         loop:true,
         nav:true,
         responsive:{
@@ -7,13 +7,32 @@ $(document).ready(function () {
                 items:1
             },
             600:{
-                items:3
+                items:4
             },
             1000:{
-                items:5
+                items:9
             }
         }
     });
+
+    $(".trending .owl-carousel").owlCarousel( {
+        center: true,
+        loop:true,
+        nav:true,
+        items:3,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+
     $( ".owl-prev").html('<i class="fa fa-arrow-left"></i>');
     $( ".owl-next").html('<i class="fa fa-arrow-right"></i>');
 
